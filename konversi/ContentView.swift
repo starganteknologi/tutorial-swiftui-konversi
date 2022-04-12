1539819ab94d7fae2711d668dc7f0e84767abf3f
Fathur Rohman <vatoer.ckplus@gmail.com>
2022-04-12 08:16:17 +0100

    Initial Commit

---
+++ konversi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  konversi
+//
+//  Created by fathur rohman on 12/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
