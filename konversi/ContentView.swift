2b089bb3d366f3d2b7031680fda19ece7677c787
Fathur Rohman <vatoer.ckplus@gmail.com>
2022-04-13 06:20:32 +0100

    selesai bs menghitung

--- konversi/ContentView.swift
+++ konversi/ContentView.swift
@@ -6,14 +6,49 @@
 //
 
 import SwiftUI
+import Collections
+
+struct KonveriPanjang {
+    var ukuran: OrderedDictionary = ["mm" : 0.001 ,"cm": 0.01 ,"m" : 1 ,"km": 1000 ,"ft": 3.2]
+    func ubah(dari: String, ke: String, input: Double) -> Double {
+        let output = ( ukuran[dari]! * input ) / ukuran[ke]!
+        return output
+     }
+}
 
 struct ContentView: View {
-    var dari: String = "m"
-    var ke: String = "m"
+    @State var dari: String = "m"
+    @State var ke: String = "m"
     @State var input: Double = 0.0
+    var konversi = KonveriPanjang()
+    var output: Double {
+        return konversi.ubah(dari: dari, ke: ke, input: input)
+    }
     var body: some View {
         Form {
-            TextField("input", value: $input, format: .number)
+            Section {
+                TextField("input", value: $input, format: .number)
+                    .keyboardType(.decimalPad)
+                Picker("pilihan input", selection: $dari){
+                    ForEach(konversi.ukuran.keys, id: \.self){
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section {
+                Picker("pilihan output", selection: $ke){
+                    ForEach(konversi.ukuran.keys, id: \.self){
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                Text(output.formatted())
+            }
+            
+            
         }
     }
 }
